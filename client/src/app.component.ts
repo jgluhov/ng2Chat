98d722630ad2447897c6f797a6d691f81cca6e2f
JGluhov <jgluhov@gmail.com>
2016-01-20 21:36:44 +0300

    add form to the page

--- client/src/app.component.ts
+++ client/src/app.component.ts
@@ -1,14 +1,40 @@
 import 'uikit/dist/css/uikit.css'
 import {Component} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {
+    FORM_DIRECTIVES,
+    ControlGroup,
+    FormBuilder,
+    AbstractControl,
+    Control
+} from 'angular2/common';
 import {RandomUserService} from './services/random.user.service';
 
 @Component({
     selector: 'ng2-chat',
-    providers: [HTTP_PROVIDERS, RandomUserService],
+    providers: [HTTP_PROVIDERS, FORM_DIRECTIVES, RandomUserService],
     template: `
-    <button class="uk-button" (click)="getRandomUser()">Get User</button>
-    <pre>{{randomUser | json}}</pre>
+    <div class="uk-container uk-container-center">
+        <div class="uk-block">
+            <form class="uk-form">
+                <fieldset>
+                <div class="uk-form-row">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" placeholder="username">
+                </div>
+                <div class="uk-form-row">
+                    <label for="password">Password</label>
+                    <input type="text" id="password" placeholder="password">
+                </div>
+                <div class="uk-form-row">
+                    <button type="submit" class="uk-button">Signin</button>
+                </div>
+                </fieldset>
+            </form>
+        </div>
+        <button class="uk-button" (click)="getRandomUser()">Get User</button>
+        <pre>{{randomUser | json}}</pre>
+    </div>
     `
 })
 
